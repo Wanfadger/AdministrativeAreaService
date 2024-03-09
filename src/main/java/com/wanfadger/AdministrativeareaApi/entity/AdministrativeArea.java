d54470f622ef632940040bbad5093aaf14f38a87
wanfadger <galiwango.fahad@gmail.com>
2024-03-09 15:04:53 +0300

    added AdministrativeArea

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/entity/AdministrativeArea.java
@@ -0,0 +1,19 @@
+package com.wanfadger.AdministrativeareaApi.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AdministrativeArea extends BaseEntity{
+
+    private String name;
+    private String code;
+    private AdministrativeAreaType administrativeAreaType;
+    private Double latitude;
+    private Double longitude;
+
+    private String partOf;//code
+}
