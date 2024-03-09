118d0ff39e1fa538ed7e488543db11f78aba1616
wanfadger <galiwango.fahad@gmail.com>
2024-03-09 15:07:55 +0300

    added InvalidException

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/shared/administrativeareaexceptions/InvalidException.java
@@ -0,0 +1,7 @@
+package com.wanfadger.AdministrativeareaApi.shared.administrativeareaexceptions;
+
+public class InvalidException extends RuntimeException{
+    public InvalidException(String message) {
+        super(message);
+    }
+}
