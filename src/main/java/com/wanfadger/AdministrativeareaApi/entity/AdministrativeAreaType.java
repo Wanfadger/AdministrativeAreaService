e4c4d81981a62a54a87531604ce2ce5e2199ba7b
wanfadger <galiwango.fahad@gmail.com>
2024-03-09 13:04:10 +0300

    added AdministrativeAreaType added all known AdministrativeAreaTypes

--- src/main/java/com/wanfadger/AdministrativeareaApi/entity/AdministrativeAreaType.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/entity/AdministrativeAreaType.java
@@ -1,4 +1,30 @@
 package com.wanfadger.AdministrativeareaApi.entity;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
 public enum AdministrativeAreaType {
+    REGION("REGION") ,//REGION
+    SUBREGION("SUB-REGION"),//SUB-REGION
+    LOCALGOVERNMENT("LOCAL GOVERNMENT"), //DISTRICT/LOCAL GOVERNMENT ,
+    COUNTY("COUNTY"), //COUNTY/CONSTITUENCY/MUNICIPALITY
+    SUBCOUNTY("SUB COUNTY"), //SUB COUNTY/TOWN COUNCIL/DIVISION
+    PARISH("PARISH") //PARISH/WARD
+    ;
+
+    private String administrativeAreaType;
+
+    AdministrativeAreaType(String administrativeAreaType) {
+        this.administrativeAreaType = administrativeAreaType;
+    }
+
+    public static Optional<AdministrativeAreaType> administrativeAreaTypeStr(String administrativeAreaTypeStr) {
+        Optional<AdministrativeAreaType> optional = Arrays.stream(AdministrativeAreaType.values()).filter(administrativeAreaType -> administrativeAreaType.administrativeAreaType.equalsIgnoreCase(administrativeAreaTypeStr)).findFirst();
+        return optional;
+    }
+
+
 }
