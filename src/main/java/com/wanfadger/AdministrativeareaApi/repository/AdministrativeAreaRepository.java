933d5285d88ce796748ee3843aaa65b392e389db
wanfadger <galiwango.fahad@gmail.com>
2024-03-09 15:06:35 +0300

    added AdministrativeAreaRepository  added findByNameIgnoreCaseAndAdministrativeAreaTypeAndPartOf  added findByPartOf  added findByAdministrativeAreaType  added findByCodeIgnoreCase  added findByNameIgnoreCase  added findAllByNameLikeIgnoreCase  added findAllByCodeLikeIgnoreCase

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/repository/AdministrativeAreaRepository.java
@@ -0,0 +1,26 @@
+package com.wanfadger.AdministrativeareaApi.repository;
+
+import com.wanfadger.AdministrativeareaApi.entity.AdministrativeArea;
+import com.wanfadger.AdministrativeareaApi.entity.AdministrativeAreaType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AdministrativeAreaRepository extends JpaRepository<AdministrativeArea , String> {
+    // uique by name , type ,partOf
+
+    Optional<AdministrativeArea> findByNameIgnoreCaseAndAdministrativeAreaTypeAndPartOf(String name , AdministrativeAreaType administrativeAreaType , String partOf);
+
+    List<AdministrativeArea> findByPartOf(String partOf);
+    List<AdministrativeArea> findByAdministrativeAreaType(AdministrativeAreaType administrativeAreaType);
+
+    Optional<AdministrativeArea> findByCodeIgnoreCase(String code);
+    Optional<AdministrativeArea> findByNameIgnoreCase(String name);
+
+    List<AdministrativeArea> findAllByNameLikeIgnoreCase(String name);
+    List<AdministrativeArea> findAllByCodeLikeIgnoreCase(String code);
+
+}
