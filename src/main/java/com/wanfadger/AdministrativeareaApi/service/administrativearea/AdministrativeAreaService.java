a1907634ad45deab373ac954d136913ab5b7a9a4
wanfadger <galiwango.fahad@gmail.com>
2024-03-10 18:41:24 +0300

    added upload service added updateOne service

--- src/main/java/com/wanfadger/AdministrativeareaApi/service/administrativearea/AdministrativeAreaService.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/service/administrativearea/AdministrativeAreaService.java
@@ -1,7 +1,9 @@
 package com.wanfadger.AdministrativeareaApi.service.administrativearea;
 
 import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaDto;
+import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaExcelDto;
 import com.wanfadger.AdministrativeareaApi.dto.NewAdministrativeAreaDto;
+import com.wanfadger.AdministrativeareaApi.dto.UpdateAdministrativeAreaDto;
 import com.wanfadger.AdministrativeareaApi.repository.projections.CodeNameProjection;
 import com.wanfadger.AdministrativeareaApi.shared.reponses.AdministrativeAreaResponseDto;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +13,8 @@ import java.util.Map;
 
 public interface AdministrativeAreaService {
 
-    ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(String type , NewAdministrativeAreaDto dto);
-    ResponseEntity<AdministrativeAreaResponseDto<String>> newList(String type , List<NewAdministrativeAreaDto> dtos);
+    ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(Map<String , String> queryMap, NewAdministrativeAreaDto dto);
+    ResponseEntity<AdministrativeAreaResponseDto<String>> newList(Map<String , String> queryMap , List<NewAdministrativeAreaDto> dtos);
 
     AdministrativeAreaResponseDto<CodeNameProjection> filterOne(Map<String ,String> queryMap);
     AdministrativeAreaResponseDto<List<CodeNameProjection>> filterList(Map<String ,String> queryMap);
@@ -20,4 +22,7 @@ public interface AdministrativeAreaService {
     AdministrativeAreaResponseDto<List<?>> searchList(Map<String ,String> queryMap);
 
 
+    AdministrativeAreaResponseDto<String> upload(List<AdministrativeAreaExcelDto> administrativeAreaExcelDtos);
+
+    AdministrativeAreaResponseDto<String> updateOne(Map<String, String> queryMap, UpdateAdministrativeAreaDto dto);
 }
