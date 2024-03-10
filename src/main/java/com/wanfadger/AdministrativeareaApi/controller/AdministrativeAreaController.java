76fc6112374b281877d8a0727ac5efb33bd4b8bc
wanfadger <galiwango.fahad@gmail.com>
2024-03-10 18:40:30 +0300

    added updateOne api updated newList api to user params

--- src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
@@ -1,7 +1,8 @@
 package com.wanfadger.AdministrativeareaApi.controller;
 
-import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaDto;
+import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaExcelDto;
 import com.wanfadger.AdministrativeareaApi.dto.NewAdministrativeAreaDto;
+import com.wanfadger.AdministrativeareaApi.dto.UpdateAdministrativeAreaDto;
 import com.wanfadger.AdministrativeareaApi.repository.projections.CodeNameProjection;
 import com.wanfadger.AdministrativeareaApi.service.administrativearea.AdministrativeAreaService;
 import com.wanfadger.AdministrativeareaApi.shared.reponses.AdministrativeAreaResponseDto;
@@ -13,20 +14,28 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/AdministrativeAreas")
 @RequiredArgsConstructor
+@CrossOrigin()
+@RequestMapping("/AdministrativeAreas")
 public class AdministrativeAreaController {
 
     private final AdministrativeAreaService administrativeAreaService;
 
-    @PostMapping("/one/{type}")
-    private ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(@PathVariable String type, @RequestBody NewAdministrativeAreaDto dto){
-        return administrativeAreaService.newOne(type,dto);
+    @PostMapping("/one")
+    private ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(@RequestParam Map<String , String> queryMap , @RequestBody NewAdministrativeAreaDto dto){
+        return administrativeAreaService.newOne(queryMap,dto);
     }
 
-    @PostMapping("/list/{type}")
-    private ResponseEntity<AdministrativeAreaResponseDto<String>> newList(@PathVariable String type , @RequestBody List<NewAdministrativeAreaDto> dtos){
-        return administrativeAreaService.newList(type , dtos);
+
+    @PutMapping("/one")
+    private AdministrativeAreaResponseDto<String> updateOne(@RequestParam Map<String , String> queryMap , @RequestBody UpdateAdministrativeAreaDto dto){
+        return administrativeAreaService.updateOne(queryMap,dto);
+    }
+
+
+    @PostMapping("/list")
+    private ResponseEntity<AdministrativeAreaResponseDto<String>> newList(@RequestParam Map<String , String> queryMap , @RequestBody List<NewAdministrativeAreaDto> dtos){
+        return administrativeAreaService.newList(queryMap , dtos);
     }
 
     @GetMapping("/filterOne")
@@ -43,6 +52,12 @@ public class AdministrativeAreaController {
     @GetMapping("/searchList")
     private AdministrativeAreaResponseDto<List<?>> searchList(@RequestParam Map<String , String> queryMap){
         return administrativeAreaService.searchList(queryMap);
+    }
+
+
+    @GetMapping("/upload")
+    private AdministrativeAreaResponseDto<String> upload(@RequestBody List<AdministrativeAreaExcelDto> administrativeAreaExcelDtos){
+        return administrativeAreaService.upload(administrativeAreaExcelDtos);
     }
 
 
