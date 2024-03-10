a255986d572c590935e76a7f8893d05c3e4e0312
wanfadger <galiwango.fahad@gmail.com>
2024-03-10 18:41:00 +0300

    added AdministrativeAreaExcelDto

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/AdministrativeAreaExcelDto.java
@@ -0,0 +1,19 @@
+package com.wanfadger.AdministrativeareaApi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdministrativeAreaExcelDto {
+    private String REGION;
+    private String SUB_REGION;
+    private String LOCAL_GOVERNMENT;
+    private String COUNTY;
+    private String SUB_COUNTY;
+    private String PARISH;
+
+
+}
