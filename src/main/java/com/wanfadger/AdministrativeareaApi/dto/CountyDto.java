a7cba29488b736aa062417a8f94461ad419b3c0f
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:26:12 +0300

    added @EqualsAndHashCode

--- src/main/java/com/wanfadger/AdministrativeareaApi/dto/CountyDto.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/CountyDto.java
@@ -2,9 +2,11 @@ package com.wanfadger.AdministrativeareaApi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class CountyDto extends AdministrativeAreaDto {
