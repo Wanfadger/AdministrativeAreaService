f9443bbd351dcc64df763567e91391cb61a98d99
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:28:02 +0300

    added UCounty

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/uniqueDtos/UCounty.java
@@ -0,0 +1,6 @@
+package com.wanfadger.AdministrativeareaApi.dto.uniqueDtos;
+
+import com.wanfadger.AdministrativeareaApi.entity.LocalGovernment;
+
+public record UCounty(String name , LocalGovernment localGovernment) {
+}
