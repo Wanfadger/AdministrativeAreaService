599f14e388501b523164ba4f577ea526568ac6e6
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:14:59 +0300

    updated AdministrativeAreaExcelDto and added alias

--- src/main/java/com/wanfadger/AdministrativeareaApi/dto/AdministrativeAreaExcelDto.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/AdministrativeAreaExcelDto.java
@@ -1,5 +1,8 @@
 package com.wanfadger.AdministrativeareaApi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wanfadger.AdministrativeareaApi.entity.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +10,34 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class AdministrativeAreaExcelDto {
-    private String REGION;
-    private String SUB_REGION;
-    private String LOCAL_GOVERNMENT;
-    private String COUNTY;
-    private String SUB_COUNTY;
-    private String PARISH;
+
+    @JsonProperty("REGION")
+    private String region;
+    private Region dbRegion;
+
+    @JsonProperty("SUB_REGION")
+    private String subRegion;
+    private SubRegion dbSubRegion;
+
+    @JsonProperty("LOCAL_GOVERNMENT")
+    private String localGovernment;
+    private LocalGovernment dbLocalGovernment;
+
+    @JsonProperty("COUNTY")
+    private String county;
+    private County dbCounty;
+
+    @JsonProperty("SUB_COUNTY")
+    private String subCounty;
+    private SubCounty dbSubCounty;
+
+    @JsonProperty("PARISH")
+    private String parish;
+    private Parish dbParish;
+
+    // upload level per get codes, then upload again
 
 
 }
