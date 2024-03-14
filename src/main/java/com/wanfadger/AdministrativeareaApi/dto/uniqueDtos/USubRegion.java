eba54bd45b54426844c5f068ee78ac39d5a30e2a
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:28:55 +0300

    added USubRegion

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/uniqueDtos/USubRegion.java
@@ -0,0 +1,6 @@
+package com.wanfadger.AdministrativeareaApi.dto.uniqueDtos;
+
+import com.wanfadger.AdministrativeareaApi.entity.Region;
+
+public record USubRegion(String name , Region region) {
+}
