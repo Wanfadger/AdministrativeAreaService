f21eb7b0cbdef237144e08680d86192ef156480d
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:26:52 +0300

    added @EqualsAndHashCode

--- src/main/java/com/wanfadger/AdministrativeareaApi/dto/RegionDto.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/RegionDto.java
@@ -1,7 +1,9 @@
 package com.wanfadger.AdministrativeareaApi.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode
 public class RegionDto extends AdministrativeAreaDto  {
 }
