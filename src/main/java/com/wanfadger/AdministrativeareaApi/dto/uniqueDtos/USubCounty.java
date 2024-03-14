3fccff1159964b6229fcbbbe35dd37b934758d64
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:28:46 +0300

    added USubCounty

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/uniqueDtos/USubCounty.java
@@ -0,0 +1,6 @@
+package com.wanfadger.AdministrativeareaApi.dto.uniqueDtos;
+
+import com.wanfadger.AdministrativeareaApi.entity.County;
+
+public record USubCounty(String name , County county) {
+}
