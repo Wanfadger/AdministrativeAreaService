a9e82c16b0412734b724ca0cdd55671eecd46ec8
wanfadger <galiwango.fahad@gmail.com>
2024-03-14 14:28:36 +0300

    added UpdateAdministrativeAreaDto

--- src/main/java/com/wanfadger/AdministrativeareaApi/dto/UpdateAdministrativeAreaDto.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/dto/UpdateAdministrativeAreaDto.java
@@ -2,9 +2,11 @@ package com.wanfadger.AdministrativeareaApi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 public   class UpdateAdministrativeAreaDto extends NewAdministrativeAreaDto {
