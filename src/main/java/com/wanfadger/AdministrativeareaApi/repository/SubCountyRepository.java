30b54e8b1744cfa6d50bba7b6dfb9b1b0dd2728c
wanfadger <galiwango.fahad@gmail.com>
2024-04-23 15:43:45 +0300

    added findAllByCountyCodes

--- src/main/java/com/wanfadger/AdministrativeareaApi/repository/SubCountyRepository.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/repository/SubCountyRepository.java
@@ -27,6 +27,9 @@ public interface SubCountyRepository extends JpaRepository<SubCounty, String> {
     @EntityGraph(attributePaths = {"county.localGovernment.subRegion.region"} , type = EntityGraph.EntityGraphType.FETCH)
     List<SubCounty> findAllByCounty_Code(String countyCode);
 
+    @Query("SELECT SC FROM SubCounty SC WHERE SC.county.code IN :countyCodes")
+    List<SubCounty> findAllByCountyCodes(List<String> countyCodes);
+
     Optional<SubCounty> findByCodeIgnoreCase(String code);
     @Query("SELECT SC.code as code , SC.name as name FROM SubCounty SC WHERE upper(SC.county.code) = upper(:county) ")
     List<CodeNameProjection> dbCodeNameList(String county);
@@ -34,4 +37,6 @@ public interface SubCountyRepository extends JpaRepository<SubCounty, String> {
 
     @Query("SELECT SC.id as code , SC.name as name FROM SubCounty SC WHERE upper(SC.code) = upper(:code) ")
     Optional<CodeNameProjection> dbCodeName(String code);
+
+
 }
