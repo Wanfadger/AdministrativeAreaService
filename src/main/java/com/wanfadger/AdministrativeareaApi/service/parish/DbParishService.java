a8a22fea3cf94fac551b9251dbc3cd86d41a8ca3
wanfadger <galiwango.fahad@gmail.com>
2024-04-23 15:41:48 +0300

    added dbBySubCountyCodes service added dbBySubCountyCodes implementation

--- src/main/java/com/wanfadger/AdministrativeareaApi/service/parish/DbParishService.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/service/parish/DbParishService.java
@@ -14,6 +14,7 @@ public interface DbParishService {
     List<Parish> dbNew(List<Parish> parishes);
     List<Parish> dbList();
     List<Parish> dbBySubCountyCode(String code);
+    List<Parish> dbBySubCountyCodes(List<String> codes);
 
     Optional<Parish> dbByName_SubCountyCode(String name , String subCountyCode);
     Optional<Parish> dbByCode(String subCountyCode);
