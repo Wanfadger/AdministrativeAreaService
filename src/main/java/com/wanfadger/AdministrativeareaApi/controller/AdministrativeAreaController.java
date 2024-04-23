7e804d084c06d8f057ca760c809e24ccf53aa832
wanfadger <galiwango.fahad@gmail.com>
2024-04-23 15:28:46 +0300

    added parishListByPartOf api

--- src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
@@ -1,9 +1,6 @@
 package com.wanfadger.AdministrativeareaApi.controller;
 
-import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaDto;
-import com.wanfadger.AdministrativeareaApi.dto.AdministrativeAreaExcelDto;
-import com.wanfadger.AdministrativeareaApi.dto.NewAdministrativeAreaDto;
-import com.wanfadger.AdministrativeareaApi.dto.UpdateAdministrativeAreaDto;
+import com.wanfadger.AdministrativeareaApi.dto.*;
 import com.wanfadger.AdministrativeareaApi.repository.projections.CodeNameProjection;
 import com.wanfadger.AdministrativeareaApi.service.administrativearea.AdministrativeAreaService;
 import com.wanfadger.AdministrativeareaApi.shared.reponses.AdministrativeAreaResponseDto;
@@ -44,9 +41,16 @@ public class AdministrativeAreaController {
         return administrativeAreaService.filterOne(queryMap);
     }
 
+
+
     @GetMapping("/filterList")
     private AdministrativeAreaResponseDto<List<CodeNameProjection>> filterList(@RequestParam Map<String , String> queryMap){
         return administrativeAreaService.filterList(queryMap);
+    }
+
+    @GetMapping("/parishListByPartOf")
+    private AdministrativeAreaResponseDto<List<CodeNameDto>> getParishByPartOf(@RequestParam Map<String , String> queryMap){
+        return administrativeAreaService.getParishByPartOf(queryMap);
     }
 
 
