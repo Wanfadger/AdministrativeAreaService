0c50a74cefb215ea33ba16657b13c73311f95fa4
wanfadger <galiwango.fahad@gmail.com>
2024-07-28 22:32:54 +0300

    enabled caching

--- src/main/java/com/wanfadger/AdministrativeareaApi/AdministrativeareaApiApplication.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/AdministrativeareaApiApplication.java
@@ -2,10 +2,12 @@ package com.wanfadger.AdministrativeareaApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class AdministrativeareaApiApplication {
 
 	public static void main(String[] args) {
