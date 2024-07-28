a5fa4f26e2ea440660b4e0511be936ee0f33f05f
wanfadger <galiwango.fahad@gmail.com>
2024-07-28 22:32:41 +0300

    cached api response

--- src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/controller/AdministrativeAreaController.java
@@ -4,7 +4,10 @@ import com.wanfadger.AdministrativeareaApi.dto.*;
 import com.wanfadger.AdministrativeareaApi.repository.projections.CodeNameProjection;
 import com.wanfadger.AdministrativeareaApi.service.administrativearea.AdministrativeAreaService;
 import com.wanfadger.AdministrativeareaApi.shared.reponses.AdministrativeAreaResponseDto;
+import com.wanfadger.AdministrativeareaApi.shared.util.CacheKeys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,58 +23,62 @@ public class AdministrativeAreaController {
     private final AdministrativeAreaService administrativeAreaService;
 
     @PostMapping("/one")
-    private ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(@RequestParam Map<String , String> queryMap , @RequestBody NewAdministrativeAreaDto dto){
-        return administrativeAreaService.newOne(queryMap,dto);
+    @CacheEvict(value = {CacheKeys.ADMINISTRATIVE_AREAS , CacheKeys.ADMINISTRATIVE_AREAS_FILTER}, allEntries = true)
+    public ResponseEntity<AdministrativeAreaResponseDto<String>> newOne(@RequestParam Map<String, String> queryMap, @RequestBody NewAdministrativeAreaDto dto) {
+        return administrativeAreaService.newOne(queryMap, dto);
+    }
+
+    @PostMapping("/list")
+    @CacheEvict(value = {CacheKeys.ADMINISTRATIVE_AREAS , CacheKeys.ADMINISTRATIVE_AREAS_FILTER}, allEntries = true)
+    public ResponseEntity<AdministrativeAreaResponseDto<String>> newList(@RequestParam Map<String, String> queryMap, @RequestBody List<NewAdministrativeAreaDto> dtos) {
+        return administrativeAreaService.newList(queryMap, dtos);
+    }
+
+    @PostMapping("/upload")
+    @CacheEvict(value = {CacheKeys.ADMINISTRATIVE_AREAS , CacheKeys.ADMINISTRATIVE_AREAS_FILTER}, allEntries = true)
+    public AdministrativeAreaResponseDto<String> upload(@RequestBody List<AdministrativeAreaExcelDto> administrativeAreaExcelDtos) {
+        return administrativeAreaService.upload(administrativeAreaExcelDtos);
     }
 
 
     @PutMapping("/one")
-    private AdministrativeAreaResponseDto<String> updateOne(@RequestParam Map<String , String> queryMap , @RequestBody UpdateAdministrativeAreaDto dto){
-        return administrativeAreaService.updateOne(queryMap,dto);
+    @CacheEvict(value = {CacheKeys.ADMINISTRATIVE_AREAS , CacheKeys.ADMINISTRATIVE_AREAS_FILTER}, allEntries = true)
+    public AdministrativeAreaResponseDto<String> updateOne(@RequestParam Map<String, String> queryMap, @RequestBody UpdateAdministrativeAreaDto dto) {
+        return administrativeAreaService.updateOne(queryMap, dto);
     }
 
-
-    @PostMapping("/list")
-    private ResponseEntity<AdministrativeAreaResponseDto<String>> newList(@RequestParam Map<String , String> queryMap , @RequestBody List<NewAdministrativeAreaDto> dtos){
-        return administrativeAreaService.newList(queryMap , dtos);
-    }
 
     @GetMapping("/filterOne")
-    private AdministrativeAreaResponseDto<CodeNameProjection> filterOne(@RequestParam Map<String , String> queryMap){
+    @Cacheable(value = CacheKeys.ADMINISTRATIVE_AREAS_FILTER, key = "#queryMap")
+    public AdministrativeAreaResponseDto<CodeNameProjection> filterOne(@RequestParam Map<String, String> queryMap) {
         return administrativeAreaService.filterOne(queryMap);
     }
 
 
-
     @GetMapping("/filterList")
-    private AdministrativeAreaResponseDto<List<CodeNameProjection>> filterList(@RequestParam Map<String , String> queryMap){
+    @Cacheable(value = CacheKeys.ADMINISTRATIVE_AREAS_FILTER, key = "#queryMap")
+    public AdministrativeAreaResponseDto<List<CodeNameProjection>> filterList(@RequestParam Map<String, String> queryMap) {
         return administrativeAreaService.filterList(queryMap);
     }
 
     @GetMapping("/parishListByPartOf")
-    private AdministrativeAreaResponseDto<List<CodeNameDto>> getParishByPartOf(@RequestParam Map<String , String> queryMap){
+    @Cacheable(value = CacheKeys.ADMINISTRATIVE_AREAS, key = "#queryMap")
+    public AdministrativeAreaResponseDto<List<CodeNameDto>> getParishByPartOf(@RequestParam Map<String, String> queryMap) {
         return administrativeAreaService.getParishByPartOf(queryMap);
     }
 
 
     @GetMapping("/searchList")
-    private AdministrativeAreaResponseDto<List<? extends AdministrativeAreaDto>> searchList(@RequestParam Map<String , String> queryMap){
+    @Cacheable(value = CacheKeys.ADMINISTRATIVE_AREAS, key = "#queryMap")
+    public AdministrativeAreaResponseDto<List<? extends AdministrativeAreaDto>> searchList(@RequestParam Map<String, String> queryMap) {
         return administrativeAreaService.searchList(queryMap);
     }
 
     @GetMapping("/searchOne")
-    private AdministrativeAreaResponseDto<? extends AdministrativeAreaDto> searchOne(@RequestParam Map<String , String> queryMap){
+    @Cacheable(value = CacheKeys.ADMINISTRATIVE_AREAS , key = "#queryMap" )
+    public AdministrativeAreaResponseDto<? extends AdministrativeAreaDto> searchOne(@RequestParam Map<String, String> queryMap) {
         return administrativeAreaService.searchOne(queryMap);
     }
-
-
-
-    @PostMapping("/upload")
-    private AdministrativeAreaResponseDto<String> upload(@RequestBody List<AdministrativeAreaExcelDto> administrativeAreaExcelDtos){
-        return administrativeAreaService.upload(administrativeAreaExcelDtos);
-    }
-
-
 
 
 }
