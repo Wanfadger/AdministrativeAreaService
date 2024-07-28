db72521f72ad3771c0d07ccbb0dfa70f41ef06de
wanfadger <galiwango.fahad@gmail.com>
2024-07-28 22:32:03 +0300

    added CacheKeys

---
+++ src/main/java/com/wanfadger/AdministrativeareaApi/shared/util/CacheKeys.java
@@ -0,0 +1,6 @@
+package com.wanfadger.AdministrativeareaApi.shared.util;
+
+public interface CacheKeys {
+    String ADMINISTRATIVE_AREAS = "AdministrativeAreas";
+    String ADMINISTRATIVE_AREAS_FILTER = "AdministrativeAreaFilters";
+}
