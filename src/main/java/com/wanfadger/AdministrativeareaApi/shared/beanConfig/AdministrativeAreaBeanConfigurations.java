8707adc0e4df38c8e4a6db8ae162fe371dba2fad
wanfadger <galiwango.fahad@gmail.com>
2026-01-06 15:46:59 +0300

    removed unused CompanyRepository and commented-out userDetailsService method from AdministrativeAreaBeanConfigurations

--- src/main/java/com/wanfadger/AdministrativeareaApi/shared/beanConfig/AdministrativeAreaBeanConfigurations.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/shared/beanConfig/AdministrativeAreaBeanConfigurations.java
@@ -2,7 +2,6 @@ package com.wanfadger.AdministrativeareaApi.shared.beanConfig;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wanfadger.AdministrativeareaApi.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -10,18 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AdministrativeAreaBeanConfigurations {
-    private final CompanyRepository companyRepository;
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        return email -> {
-//            Company company = new Company();
-//            company.setEmail("test");
-//            company.setName("test");
-//            //companyRepository.findByEmailIgnoreCase(email).orElseThrow(() -> new NotFoundException("Email profile not found"));
-//            return new User(company.getEmail() , "", company.isEnabled() , company.isAccountNonExpired() , company.isCredentialsNonExpired() , company.isAccountNonLocked() , Collections.EMPTY_LIST);
-//        }; //load user;
-//    }
 
     @Bean
     public ObjectMapper objectMapper(){
