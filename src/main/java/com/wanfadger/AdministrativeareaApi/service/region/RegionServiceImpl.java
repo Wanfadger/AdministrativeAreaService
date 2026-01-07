17f53f386bdcd17aa3e57e0677832d4f4b227226
wanfadger <galiwango.fahad@gmail.com>
2026-01-07 18:28:43 +0300

    Remove commented-out methods from RegionServiceImpl to improve code clarity
    - Deleted all commented-out methods in the `RegionServiceImpl` class to streamline the code and enhance readability.
    - This cleanup helps maintain a cleaner codebase by eliminating unused and unnecessary code.
    
    Status: Enhanced code clarity by removing obsolete commented-out methods.

--- src/main/java/com/wanfadger/AdministrativeareaApi/service/region/RegionServiceImpl.java
+++ src/main/java/com/wanfadger/AdministrativeareaApi/service/region/RegionServiceImpl.java
@@ -15,54 +15,6 @@ public class RegionServiceImpl implements DbRegionService  {
 
     final RegionRepository regionRepository;
 
-//    @Override
-//    @Transactional
-//    public SiipResponseDto<RegionDtos.RegionDto> newRegion(RegionDtos.NewDto dto) {
-//        Optional<Region> regionOptional = dbByName(dto.getName());
-//        if (regionOptional.isPresent()) {
-//            throw new AlreadyExistsException("Region already exists");
-//        }
-//
-//        Region region = new Region();
-//        region.setCode(dto.getCode());
-//        region.setName(dto.getName());
-//        region.setDescription(dto.getDescription());
-//
-//        Region dbNewRegion = dbNew(region);
-//
-//        RegionDtos.RegionDto regionDto = new RegionDtos.RegionDto(dbNewRegion.getId() , dbNewRegion.getCode(), dbNewRegion.getName(), dbNewRegion.getDescription());
-//
-//        return new SiipResponseDto<>(regionDto , "success");
-//    }
-//
-//    @Override
-//    public SiipResponseDto<List<RegionDtos.RegionDto>> regions() {
-//        List<RegionDtos.RegionDto> dtoList = dbList().stream().map(region -> new RegionDtos.RegionDto(region.getId(), region.getCode(), region.getName(), region.getDescription())).toList();
-//        return new SiipResponseDto<>(dtoList , "success");
-//    }
-//
-//    @Override
-//    @Transactional
-//    public SiipResponseDto<String> updateRegion(String regionId, RegionDtos.NewDto dto) {
-//        Region region = dbById(regionId).orElseThrow(() -> new NotFoundException("Region not found"));
-//
-//        if(notNullEmpty(dto.getName())){
-//            region.setName(dto.getName());
-//        }
-//
-//        if(notNullEmpty(dto.getCode())){
-//            region.setCode(dto.getCode());
-//        }
-//
-//        if(notNullEmpty(dto.getDescription())){
-//            region.setDescription(dto.getDescription());
-//        }
-//
-//        dbNew(region);
-//
-//        return new SiipResponseDto<>("success" , "success");
-//    }
-
 
     @Override
     public Region dbNew(Region region) {
